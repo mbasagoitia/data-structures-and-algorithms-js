f5b23403e9a455fbdf765a3c96bc8a6060d91b5e
basagoitiam <marika.basagoitia@gmail.com>
2024-02-12 10:35:06 -0500

    implemented recursive solution to binary search

--- searching-algorithms/binarySearch.js
+++ searching-algorithms/binarySearch.js
@@ -21,3 +21,26 @@ function binarySearch (arr, target) {
 }
 
 // Big-O: O(logn) (logarithmic)
+
+// Recursive solution
+
+function binarySearchRecursive (arr, target) {
+    return search (arr, target, 0, arr.length - 1)
+}
+
+function search (arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+        return -1;
+    }
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+        return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex - 1);
+    } else {
+        return search(arr, target, middleIndex + 1, rightIndex);
+    }
+}
+
+// Big-O: O(logn) (logarithmic)
