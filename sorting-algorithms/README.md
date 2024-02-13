2254b6183e8f883c44e6ace97afde55a9f437255
basagoitiam <marika.basagoitia@gmail.com>
2024-02-13 12:07:16 -0500

    implemented bubble sort algorithm and determined big-o time complexity

---
+++ sorting-algorithms/README.md
@@ -0,0 +1,10 @@
+# Sorting Algorithms
+
+Often in web development, you may sort items by newest first or by most number of upvotes.
+
+## Four Types of Sorting Algorithms
+
+- Bubble sort
+- Insertion sort
+- Quick sort
+- Merge sort
---
+++ sorting-algorithms/bubbleSort.js
@@ -0,0 +1,33 @@
+// This is a poor sorting algorithm and should generally not be used in
+// a real-world situation.
+
+// Bubble sort compares adjacent elements in the array and swaps their positions
+// if they are not in the intended order.
+
+// If at least one swap has occurred, the process will repeat until the entire array is sorted.
+// The large number "bubbles" to the top, hence the name.
+
+// Use of a temporary variable "temp" is useful for swapping array indices
+
+// Make sure to have the swapped = false in the correct place (right at the beginning
+// of the do...while loop, not at the beginning of each iteration of the for loop).
+
+// The for loop only needs to compare up to arr.length - 1 because we always check the next index, so the
+// last element is already covered.
+
+function bubbleSort (arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+// Big-O: O(n^2) (quadratic)
