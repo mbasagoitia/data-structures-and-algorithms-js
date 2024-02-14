faff42d66c5a8ee925f424160fdff720665be0d5
basagoitiam <marika.basagoitia@gmail.com>
2024-02-14 13:25:57 -0500

    added notes for quick sort

---
+++ sorting-algorithms/quickSort.js
@@ -0,0 +1,11 @@
+// Identify the pivot element in the array (can be first, last, median, random element)
+// We will use the last element as the pivot in this solution
+
+// Traverse the entire array and put everything that is smaller than the pivot
+// into a "left" array and everything greater than the pivot into a "right" array
+
+// Repeat the process for the individual left and right arrays until you have an array of length 1
+// which is sorted by definition
+
+// Repeatedly concatenate the left array, pivot, and right array until you have one sorted array
+
